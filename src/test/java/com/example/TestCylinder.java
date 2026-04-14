de5beb54260fcc3233de10dc6f19864941bb1e58
Szász Benedek <benedekszasz7@gmail.com>
2026-04-14 15:19:30 +0200

    Tesztírások

--- src/test/java/com/example/TestCylinder.java
+++ src/test/java/com/example/TestCylinder.java
@@ -4,7 +4,26 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestCylinder {
-    @Test public void testCalcSurfaceData1() {
+    @Test public void testCalcSurface_30_35_12252() {
+        double expected = 12252.21;
+        double actual = Cylinder.calcSurface(30, 35);
+        Assert.assertEquals(actual, expected, 0.1);
+    }
+    @Test public void testCalcSurface_132_48_149288() {
+        double expected = 149288.48;
+        double actual = Cylinder.calcSurface(132, 48);
+        Assert.assertEquals(actual, expected, 0.1);
+    }
+    @Test public void testCalcSurface_1_35_226() {
+        double expected = 226.19;
+        double actual = Cylinder.calcSurface(1, 35);
+        Assert.assertEquals(actual, expected, 0.1);
+        Assert.assertTrue(true);
+    }
+    @Test public void testCalcSurface_0_27() {
+        double expected = 226.19;
+        double actual = Cylinder.calcSurface(1, 35);
+        Assert.assertEquals(actual, expected, 0.1);
         Assert.assertTrue(true);
     }
 }
